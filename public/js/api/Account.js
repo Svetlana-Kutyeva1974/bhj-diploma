e7ab6843faec33befc16c6c50f13a409629bd0df
Svetlana-Kutyeva1974 <81332075+Svetlana-Kutyeva1974@users.noreply.github.com>
2021-09-17 01:55:10 +0300

    1

--- public/js/api/Account.js
+++ public/js/api/Account.js
@@ -4,10 +4,33 @@
  * Имеет свойство URL со значением '/account'
  * */
 class Account extends Entity {
+  static URL = '/account';//может в конструкторе добавить ...args super(...args)
+  //потому что другой url 
   /**
    * Получает информацию о счёте
    * */
   static get(id = '', callback){
+
+    createRequest({
+    url: URL, // адрес
+    data: { // произвольные данные, могут отсутствовать
+      email: data.email,
+      password: data.password
+    },
+    method: 'GET', // метод запроса
+    /*
+      Функция, которая сработает после запроса.
+      Если в процессе запроса произойдёт ошибка, её объект
+      должен быть в параметре err.
+      Если в запросе есть данные, они должны быть переданы в response.
+    */
+    callback: (err, response) => {
+      console.log( 'Ошибка, если есть', err );
+      console.log( 'Данные, если нет ошибки', response );
+
+      // id что -то с ним нужно сделать
+    }
+  });
 
   }
 }
--- public/js/api/User.js
+++ public/js/api/User.js
@@ -4,12 +4,14 @@
  * Имеет свойство URL, равное '/user'.
  * */
 class User {
+  static URL = '/user';
   /**
    * Устанавливает текущего пользователя в
    * локальном хранилище.
    * */
   static setCurrent(user) {
-
+     localStorige.setItem('user', JSON.stringify(user));
+     console.log( localStorage.user);
   }
 
   /**
@@ -18,6 +20,12 @@ class User {
    * */
   static unsetCurrent() {
 
+    //User.setCurrent( user );
+    //let current = User.current();
+    //console.log( current );
+
+    localStorage.removeItem('user');
+
   }
 
   /**
@@ -25,7 +33,8 @@ class User {
    * из локального хранилища
    * */
   static current() {
-
+    let userCurrent = localStorage.getItem(this);
+    return (JSON.parse(userCurrent) || undefined) ;
   }
 
   /**
@@ -33,9 +42,26 @@ class User {
    * авторизованном пользователе.
    * */
   static fetch(callback) {
+    createRequest({
+      url: this.URL + '/current',
+      method: 'GET',
+      responseType: 'json',
+      //data,
+      callback: (err, response) => {
+        if (response && response.user) {
+          this.setCurrent(response.user);
+        }
+        else {
+          callback(err, response);
+          this.unsetCurrent();
+        }
+        //callback(err, response);
+      }
+    });
 
   }
 
+  //////////////////////////это было до меня
   /**
    * Производит попытку авторизации.
    * После успешной авторизации необходимо
@@ -56,6 +82,7 @@ class User {
       }
     });
   }
+  //////////////////////////это было до меня
 
   /**
    * Производит попытку регистрации пользователя.
@@ -64,6 +91,23 @@ class User {
    * User.setCurrent.
    * */
   static register(data, callback) {
+    createRequest({
+      url: this.URL + '/register',
+      method: 'POST',
+      responseType: 'json',
+      /*data: {
+        name: data.name,
+        email: data.email,
+        password: data.password
+      }*/
+      data,
+      callback: (err, response) => {
+        if (response && response.user) {
+          this.setCurrent(response.user);
+        }
+        callback(err, response);
+      }
+    });
 
   }
 
@@ -75,3 +119,191 @@ class User {
 
   }
 }
+
+
+
+
+
+
+
+
+//---
+
+/*
+class LogoutButton {
+  constructor() {
+    [this.logoutBtn] = document.getElementsByClassName('logout');
+    this.action = (f) => f;
+    this.logoutBtn.addEventListener('click', this.logoutClick.bind(this));
+  }
+
+  logoutClick(event) {
+    event.preventDefault();
+    this.action();
+  }
+}*/
+
+
+
+
+
+
+/*
+
+class UserForm {
+  constructor() {
+    this.loginForm = document.getElementById('login');
+    this.registerForm = document.getElementById('register');
+    this.loginForm.querySelector('.button').addEventListener('click', this.loginFormAction.bind(this));
+    this.registerForm.querySelector('.button').addEventListener('click', this.registerFormAction.bind(this));
+
+    this.loginErrorMessageBox = this.loginForm.querySelector('.ui.message');
+    this.loginErrorMessageBox.style.display = 'none';
+    this.registerErrorMessageBox = this.registerForm.querySelector('.ui.message');
+    this.registerErrorMessageBox.style.display = 'none';
+
+    this.loginFormCallback = (f) => f;
+    this.registerFormCallback = (f) => f;
+  }
+
+  setLoginErrorMessage(message) {
+    this.loginErrorMessageBox.innerText = message;
+    this.loginErrorMessageBox.style.display = 'block';
+    setTimeout(() => { this.loginErrorMessageBox.style.display = 'none'; }, 5000);
+  }
+
+  setRegisterErrorMessage(message) {
+    this.registerErrorMessageBox.innerText = message;
+    this.registerErrorMessageBox.style.display = 'block';
+    setTimeout(() => { this.registerErrorMessageBox.style.display = 'none'; }, 5000);
+  }
+
+  loginFormAction(event) {
+    event.preventDefault();
+    this.loginFormCallback(this.getData(this.loginForm));
+    this.loginForm.reset();
+  }
+
+  registerFormAction(event) {
+    event.preventDefault();
+    this.registerFormCallback(this.getData(this.registerForm));
+    this.registerForm.reset();
+  }
+
+  getData(form) {
+    const login = form.querySelector('[name="email"]').value;
+    const password = form.querySelector('[name="password"]').value;
+    return { login, password };
+  }
+}
+
+*/
+/*
+
+
+static login({ login, password }, callback) {
+    const asyncPart = async () => {
+      const body = JSON.stringify({ login, password });
+
+      const response = await fetch('/user/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body,
+      });
+
+      const result = await ApiConnector.parseResponseBody(response);
+      return result;
+    };
+
+    asyncPart()
+      .then(({ responseBody }) => {
+        callback(responseBody);
+      })
+      .catch((e) => {
+        console.error("Произошла ошибка: ", e);
+      });
+  }
+
+
+*/
+  /**
+   * Отправляет запрос на создание пользователя с переданными параметрами
+   *
+   //* @static
+  // * @param {*} { username, password }
+  /// * @param {Function} callback-функция с телом `data` в качестве параметра
+  // * @memberof ApiConnector
+   */
+  /*static register({ login, password }, callback) {
+    const asyncPart = async () => {
+      const body = JSON.stringify({ login, password });
+
+      const response = await fetch('user/register', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body,
+      });
+      const result = await ApiConnector.parseResponseBody(response);
+      return result;
+    };
+    asyncPart()
+      .then(({ responseBody }) => {
+        callback(responseBody);
+      })
+      .catch((e) => {
+        console.error("Произошла ошибка: ", e);
+      });
+  }
+*/
+  /**
+   * Отправляет запрос на получение текущего авторизованного пользователя
+   *
+   * //@static
+   * //@param {Function} callback-функция с телом `data` в качестве параметра
+   //* @memberof ApiConnector
+   */
+  /*static current(callback) {
+    const asyncPart = async () => {
+      const response = await fetch('user/current', {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+      });
+      const result = await ApiConnector.parseResponseBody(response);
+      return result;
+    };
+    asyncPart()
+      .then(({ responseBody }) => {
+        callback(responseBody);
+      })
+      .catch((e) => {
+        console.error("Произошла ошибка: ", e);
+      });
+  }
+*/
+  /**
+   //* Отправляет запрос деавторизацию пользователя
+   *
+  // * @static
+  // * @param {Function} callback-функция с телом `data` в качестве параметра
+  // * @memberof ApiConnector
+   */
+  /*static logout(callback) {
+    const asyncPart = async () => {
+      const response = await fetch('user/logout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+      });
+      const result = await ApiConnector.parseResponseBody(response);
+      return result;
+    };
+    asyncPart()
+      .then(({ responseBody }) => {
+        callback(responseBody);
+      })
+      .catch((e) => {
+        console.error("Произошла ошибка: ", e);
+      });
+  }
+
+
+*/
