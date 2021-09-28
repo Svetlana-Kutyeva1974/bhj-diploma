18ab730c7a4f02511a041ab0063af87000c77824
Svetlana-Kutyeva1974 <81332075+Svetlana-Kutyeva1974@users.noreply.github.com>
2021-09-28 03:24:28 +0300

    2

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -2,39 +2,25 @@
  * Основная функция для совершения запросов
  * на сервер.
  * */
-                                            //const createRequest = (options = {}) => {
-                                            //const createRequest = (options = {url, data= 'null', method}) => {
+                                                                            
 const createRequest = (options = {}) => {
 let url = options.url;
 let method = options.method;
 let callback = options.callback;
-let data = options.data;
-                                             //const { url, data, method} = option;
+let data = options.data;                                
 
 const xhr = new XMLHttpRequest;
 xhr.responseType = 'json';
 
-                                            //xhr.open( `${method}`, `${url?mail=data.mail&password=data.password}` ); 
+                                            
   try {
-  
-    //xhr.open( method, `url?email=data.email&password=data.password` );
-    xhr.open( method, url );
-   // xhr.open( method, `url?mail=data.mail&password=data.password` );
-
-    //  xhr.open( method, `url?mail=data.mail&password=data.password` );
-   // xhr.open( method, `${url}?mail=${data.mail}&password=${data.password}` );
-   // xhr.open( method, `${url}?mail=${options.mail}&password=${data.password}` );
     if (method === `GET`){
-      xhr.send( data );
+      xhr.open( method, `url?email=data.email&password=data.password` );
+      xhr.send();
     }
     else {
-      const formData = new FormData();
-      //formData.append( 'email', data.email );//formData.append( 'mail', data.mail );
-     // formData.append( 'password', data.password );
-
-      formData.append('data', data);
-       xhr.send( formData );
-      //xhr.send( data );
+       xhr.open( method, url );
+       xhr.send( data );
     }
 
   }
@@ -62,163 +48,3 @@ xhr.onerror = function() {
 };
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//---------------------------------------------
-
-
- /* if (data != "" ){
-      xhr.open( method, url );
-      xhr.send( data );
-      }
-    else {
-      xhr.open( method, url );
-      xhr.send();
-
-    }*/
-   // xhr.open( `${method}`, `${url?mail=data.mail&password=data.password}` );
-   // xhr.open( method, url );
-
-
-
-/*
-  // ...
-  const xhr = new XMLHttpRequest;
-  xhr.open( 'GET', 'http://localhost:8000/public/js?mail=mail&password=password' );
-  xhr.send();
-  // ...
-  try {
-    xhr.open( method, url );
-    xhr.send( data );
-  }
-  catch ( e ) {
-    // перехват сетевой ошибки
-    callback( e );
-  }
-  */
-
-
-
-/*
-
-  createRequest({
-    url: 'https://example.com', // адрес
-    data: { // произвольные данные, могут отсутствовать
-      email: 'ivan@poselok.ru',
-      password: 'odinodin'
-    },
-    method: 'GET', // метод запроса
-    //
-      Функция, которая сработает после запроса.
-      Если в процессе запроса произойдёт ошибка, её объект
-      должен быть в параметре err.
-      Если в запросе есть данные, они должны быть переданы в response.
-    //
-    callback: (err, response) => {
-      console.log( 'Ошибка, если есть', err );
-      console.log( 'Данные, если нет ошибки', response );
-    }
-  });
-
-*/
-
-
-
-
-
-
-/*
-
-В случае успешного выполнения кода, необходимо вызвать функцию, заданную в callback и передать туда данные:
-
-// при успешном выполнении
-  createRequest({
-    url: 'https://example.com',
-    method: 'GET',
-    callback: ( err, response ) => {
-      
-        //при успешном выполнении err = null, response содержит данные ответа
-      
-      console.log( err ); // null
-      console.log( response ); // ответ
-    }
-  });*/
-
-// при ошибке
- /* createRequest({
-    url: 'https://example.com',
-    method: 'GET',
-    callback: ( err, response ) => {
-      console.log( err ); // объект ошибки
-    }
-  });
-*/
-
-
-
-
-
-
-
-
-/*
-{login, password}
-xhr.open(method, URL, [async, user, password])
-let formData = new FormData(message);
-let xhr = new XMLHttpRequest();
-
-// 1. Создаём новый XMLHttpRequest-объект
-let xhr = new XMLHttpRequest();
-
-// 2. Настраиваем его: GET-запрос по URL /article/.../load
-xhr.open('GET', '/article/xmlhttprequest/example/load');
-
-
-//!!!xhr.responseType = 'json'; //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-// 3. Отсылаем запрос
-xhr.send();
-
-// 4. Этот код сработает после того, как мы получим ответ сервера
-xhr.onload = function() {
-  if (xhr.status != 200) { // анализируем HTTP-статус ответа, если статус не 200, то произошла ошибка
-    alert(`Ошибка ${xhr.status}: ${xhr.statusText}`); // Например, 404: Not Found
-  } else { // если всё прошло гладко, выводим результат
-    alert(`Готово, получили ${xhr.response.length} байт`); // response -- это ответ сервера
-  }
-};
-
-xhr.onprogress = function(event) {
-  if (event.lengthComputable) {
-    alert(`Получено ${event.loaded} из ${event.total} байт`);
-  } else {
-    alert(`Получено ${event.loaded} байт`); // если в ответе нет заголовка Content-Length
-  }
-
-};
-
-xhr.onerror = function() {
-  alert("Запрос не удался");
-};
-
-
-
-
-
-
-*/
